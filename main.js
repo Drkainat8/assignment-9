5c4a3dcf526e6b7d54384766e53176179492d40e
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-05-15 23:41:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//assignment 9 make a variable contain your fav number than print a message that contain your fav number//
+var favouritenumber = 6;
+console.log("my favourite number is ".concat(favouritenumber));
---
+++ main.ts
@@ -0,0 +1,3 @@
+//assignment 9 make a variable contain your fav number than print a message that contain your fav number//
+let favouritenumber : number = 6;
+console.log(`my favourite number is ${favouritenumber}`)
